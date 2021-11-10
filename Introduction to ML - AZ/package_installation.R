11948d70cd6e7d8111b003444c5fb3d1aba0e889
elliecwilliams <ecw63@cam.ac.uk>
2021-11-10 10:28:32 +0000

    Added package installation, caret html and correlation heatmap

---
+++ Introduction to ML - AZ/package_installation.R
@@ -0,0 +1,69 @@
+#!/usr/bin/env Rscript
+
+cran.packages<-c("caret",
+                 "tidyverse",
+                 "rpart",
+                 "rpart.plot",
+                 "party",
+                 "randomForest",
+                 "e1071",
+                 "Rtsne",
+                 "dbscan",
+                 "C50",
+                 "UpSetR",
+                 "RColorBrewer",
+                 "GGally",
+                 "ggfortify",
+                 "reshape2",
+                 "plyr",
+                 "corrplot",
+                 "pROC",
+                 "scatterplot3d",
+                 "dendextend", 
+                 "magrittr",
+                 "cluster", 
+                 "gplots",
+                 "methods", 
+                 "class",
+                 "datasets", 
+                 "caTools",
+                 "ggplot2",
+                 "ggdendro",
+                 "doParallel",
+                 "devtools",
+                 "mlbench",
+                 "plot3D",  
+                 "ROCR",
+                 "UsingR",
+                 "rafalib",
+                 "downloader",
+                 "lattice",
+                 "stepPlr",
+                 "arm", 
+                 "kernlab",
+                 "nnet",
+                 "neuralnet",
+                 "MASS",
+                 "NeuralNetTools",
+                 "ISLR", 
+                 "boot",
+                 "faraway",
+                 "CVST", 
+                 "readr")
+
+bioconductor.packages<-c("EBImage")
+
+#Installing CRAN packages
+new.packages <- cran.packages[!(cran.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#Installing bioconductor packages
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install()
+new.bio.packages<-bioconductor.packages[!(bioconductor.packages %in% installed.packages()[,"Package"])]
+if(length(new.bio.packages)) BiocManager::install(new.bio.packages)
+
+# install packages from other repositories
+devtools::install_github("SheffieldML/vargplvm/vargplvmR")
+devtools::install_github("ManchesterBioinference/DEtime")
