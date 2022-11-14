b55ec9c1ed55af4081f5b97c276db734e441353a
elliecwilliams <ecw63@cam.ac.uk>
2022-11-14 17:32:56 +0000

    Adding October 2022 changes to course, adding new practicals

--- Introduction to ML - AZ/package_installation.R
+++ Introduction to ML - AZ/package_installation.R
@@ -65,6 +65,66 @@ BiocManager::install()
 new.bio.packages<-bioconductor.packages[!(bioconductor.packages %in% installed.packages()[,"Package"])]
 if(length(new.bio.packages)) BiocManager::install(new.bio.packages)
 
+for (package in cran.packages){
+  eval(bquote(library(.(package))))
+}
+
+for (package in bioconductor.packages){
+  eval(bquote(library(.(package))))
+}
+
 # install packages from other repositories
-devtools::install_github("SheffieldML/vargplvm/vargplvmR")
 devtools::install_github("ManchesterBioinference/DEtime")
+
+
+library("caret",
+library("gmodels",
+library("tidyverse",
+library("rpart",
+library("rpart.plot",
+library("party",
+library("randomForest",
+library("e1071",
+library("Rtsne",
+library("dbscan",
+library("C50",
+library("UpSetR",
+library("RColorBrewer",
+library("GGally",
+library("ggfortify",
+library("reshape2",
+library("plyr",
+library("corrplot",
+library("pROC",
+"scatterplot3d",
+"dendextend", 
+"magrittr",
+"cluster", 
+"gplots",
+"methods", 
+"class",
+"datasets", 
+"caTools",
+"ggplot2",
+"ggdendro",
+"doParallel",
+"devtools",
+"mlbench",
+"plot3D",  
+"ROCR",
+"UsingR",
+"rafalib",
+"downloader",
+"lattice",
+"stepPlr",
+"arm", 
+"kernlab",
+"nnet",
+"neuralnet",
+"MASS",
+"NeuralNetTools",
+"ISLR", 
+"boot",
+"faraway",
+"CVST", 
+"readr")
